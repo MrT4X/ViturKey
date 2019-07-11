4ea5707add7b0bc3824f8f26cbd333306fc65be6
MrT4X <51880140+MrT4X@users.noreply.github.com>
2019-07-11 21:36:36 +0700

    Update MrT4x.py

--- MrT4x.py
+++ MrT4x.py
@@ -7,11 +7,10 @@ b ='\033[91m'
 c ='\033[0m'
 os.system('clear')
 print(a+'\t  Tombol Tambahan Termux ')
-print(a+'\t  UP,Down,right,Left, CTRL ')
-print(b+'\t  Bye:   Kumpulan Remaja')
-print('\t web : Kumpulanremaja.com')
-print('\t Facebook : fb.me/4Kumpulanremaja')
-print('\t Github : https://github.com/kumpulanremaja')
+print(a+'\t  UP,Down,right,Left, CTRL, ENTER ')
+print(b+'\t  Bye:   Mr.T4x')
+print('\t Facebook : AnomsHacker')
+print('\t Github : https://github.com/MrT4X')
 print(a+'+'*40)
 print('\nProses..')
 sleep(1)
@@ -26,7 +25,7 @@ sleep(1)
 print(b+'\n[!] menambahkan File Tambahan..')
 sleep(1)
 
-key = "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]"
+key = "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN','ENTER']]"
 kontol = open('/data/data/com.termux/files/home/.termux/termux.properties','w')
 kontol.write(key)
 kontol.close()
